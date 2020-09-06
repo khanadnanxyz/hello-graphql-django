0989e41103d800d3fae9f3b64f86c335d8f059d8
Adnan Ahmed Khan <iamkhanadnan@gmail.com>
2020-09-06 14:02:43 +0600

    Implements Likes to Tracks

--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -2,7 +2,8 @@ import graphene
 from graphene_django import DjangoObjectType
 from graphql import GraphQLError
 
-from .models import Track
+from .models import Track, Like
+from users.schema import UserType
 
 
 class TrackType(DjangoObjectType):
@@ -10,11 +11,20 @@ class TrackType(DjangoObjectType):
         model = Track
 
 
+class LikeType(DjangoObjectType):
+    class Meta:
+        model = Like
+
+
 class Query(graphene.ObjectType):
     tracks = graphene.List(TrackType)
+    likes = graphene.List(LikeType)
 
     def resolve_tracks(self, info):
         return Track.objects.all()
+
+    def resolve_likes(self, info):
+        return Like.objects.all()
 
 
 class CreateTrack(graphene.Mutation):
@@ -80,8 +90,33 @@ class DeleteTrack(graphene.Mutation):
         return DeleteTrack(track_id=track_id)
 
 
+class CreateLike(graphene.Mutation):
+    user = graphene.Field(UserType)
+    track = graphene.Field(TrackType)
+
+    class Arguments:
+        track_id = graphene.Int(required=True)
+
+    def mutate(self, info, track_id):
+        user = info.context.user
+        if user.is_anonymous:
+            raise GraphQLError('Login to like tracks.')
+
+        track = Track.objects.get(id=track_id)
+        if not track:
+            raise GraphQLError('Cannot find track with given track id')
+
+        Like.objects.create(
+            user=user,
+            track=track
+        )
+
+        return CreateLike(user=user, track=track)
+
+
 class Mutation(graphene.ObjectType):
     create_track = CreateTrack.Field()
     update_track = UpdateTrack.Field()
     delete_track = DeleteTrack.Field()
+    create_like = CreateLike.Field()
 
--- app/users/schema.py
+++ app/users/schema.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 
 import graphene
 from graphene_django import DjangoObjectType
+from graphql import GraphQLError
 
 
 class UserType(DjangoObjectType):
@@ -19,7 +20,7 @@ class Query(graphene.ObjectType):
     def resolve_me(self, info):
         user = info.context.user
         if user.is_anonymous:
-            raise Exception('Not logged in!')
+            raise GraphQLError('Not logged in!')
         return user
 
 
